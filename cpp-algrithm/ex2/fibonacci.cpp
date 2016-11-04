1976d6335e34c73c8d85f338df97746ae1321976
orange-beans <cczhang@simtech.a-star.edu.sg>
2016-11-04 14:53:34 +0800

    Add Advance Fibonacci algrithms

--- cpp-algrithm/ex2/fibonacci.cpp
+++ cpp-algrithm/ex2/fibonacci.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <vector>
 
 //int Fibonacci(int n);
 
@@ -10,6 +12,7 @@ long int Fibonacci(long int n) {
   }
 }
 
+// O(n^2)
 long long FibonacciFast(long long n) {
   int temp_1 = 0, temp_2 = 1, result = 0;
   for(int i = 2; i < n+1 ; i++) {
@@ -22,10 +25,57 @@ long long FibonacciFast(long long n) {
   return result;
 }
 
+// Last digit of a Large Fibonacci Number
+long long LastDigitOfFinonacci(long long n) {
+  int temp_1 = 0, temp_2 = 1, result = 0;
+  for(int i = 2; i < n+1 ; i++) {
+    // Calculate current
+    result = (temp_1 + temp_2)%10;
+    // Update temp_1 and temp_2
+    temp_1 = temp_2;
+    temp_2 = result;
+  }
+  return result;
+}
+
+// Huge Fibonacci Number modulom
+// Step 1. Find the Pisano period (results will repeat)
+// Step 2. Find n % period, then lookup in the period table
+long long HugeFinonacciModul(long long n, long long m) {
+  // Step 1:
+  std::vector<long long> table;
+  table.push_back(0);
+  table.push_back(1);
+  int i = 2, period = 0;
+  //std::cout << 0 << " : " << table[0] << std::endl;
+  //std::cout << 1 << " : " << table[1] << std::endl;
+  while(true) {
+    // Find Fibonacci Number
+    table.push_back((table[i-1] + table[i-2])%m);
+    //table[i] = (table[i-1] + table[i-2])%m;
+    //std::cout << i << " : " << table[i] << std::endl;
+    // Store after module
+    if(table[i]==1 && table[i-1]==0) break;
+    i++;
+  }
+  table.resize(i-1);
+  // Step 2:
+  period = table.size();
+  return table[n % period];
+}
+
 int main(){
-  long int n = 0;
+  long long n = 0, m = 0;
   std::cin >> n;
-  std::cout << Fibonacci(n);
-  //std::cout << FibonacciFast(n);
+  std::cin >> m;
+  clock_t tStart = clock();
+  // Finonacci Number
+  //std::cout << Fibonacci(n);
+  //std::cout << FibonacciFast(n) << std::endl;
+  // Last digit of a large Finonacci Number
+  //std::cout << LastDigitOfFinonacci(n) << std::endl;
+  // Huge Finonacci Number modulom
+  std::cout << HugeFinonacciModul(n, m) << std::endl;
+  std::cout << "Time taken:\n" << (double)(clock() - tStart)/CLOCKS_PER_SEC << " s" << std::endl;
   return 0;
 }
--- cpp-algrithm/ex2/gcd.cpp
+++ cpp-algrithm/ex2/gcd.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <time.h>
 
+// Greatest Common Divisor
 long long EuclidGCD(long long a, long long b) {
   if (b == 0) {
     return a;
@@ -8,11 +10,24 @@ long long EuclidGCD(long long a, long long b) {
   }
 }
 
+// Least Common Multiple
+// 1. Find the GCD of A and B
+// 2. A*B/GCD
+long long LCM(long long a, long long b) {
+  long long gcd = EuclidGCD(a, b);
+  return a*b/gcd;
+}
+
+
 int main(){
   long long a = 0, b = 0;
   std::cin >> a;
   std::cin >> b;
-  //std::cout << Fibonacci(n);
-  std::cout << EuclidGCD(a, b);
+  clock_t tStart = clock();
+  // Greatest Common Divisor
+  //std::cout << EuclidGCD(a, b) << std::endl;
+  // Least Common Multiple
+  std::cout << LCM(a, b) << std::endl;
+  std::cout << "Time taken:\n" << (double)(clock() - tStart)/CLOCKS_PER_SEC << " s" << std::endl;
   return 0;
 }
