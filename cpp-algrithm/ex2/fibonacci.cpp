0f51f97f0146ddcb8527d492977c732326c35250
orange-beans <cczhang@simtech.a-star.edu.sg>
2016-11-04 15:34:03 +0800

    Add SumOfFibonacci algrithms

--- cpp-algrithm/ex2/fibonacci.cpp
+++ cpp-algrithm/ex2/fibonacci.cpp
@@ -64,18 +64,50 @@ long long HugeFinonacciModul(long long n, long long m) {
   return table[n % period];
 }
 
+// Sum of Fibonacci Numbers
+// Analysis: Sum = F(n+2) - F(1) = F(n+2) - 1;
+// Step 1: Find F(n+2), last digital modulom by 10;
+// Step 2: return result - 1
+long long SumOfFibonacci(long long n) {
+  long long n_plus_2 = HugeFinonacciModul(n+2, 10);
+  n_plus_2 = n_plus_2 == 0 ? 10 : n_plus_2;
+  return n_plus_2 - 1;
+}
+
+// Partial Sum of Fibonacci Numbers
+// Analysis: Sum = F(n+2) - F(m+1);
+// Step 1: Find F(n+2) & F(m+1), last digital modulom by 10;
+// Step 2: return result;
+long long PartialSumOfFibonacci(long long n, long long m) {
+  long long n_plus_2 = HugeFinonacciModul(n+2, 10);
+  long long m_plus_1 = HugeFinonacciModul(m+1, 10);
+  if (n_plus_2 < m_plus_1) n_plus_2 += 10;
+  return n_plus_2 - m_plus_1;
+}
+
+
 int main(){
   long long n = 0, m = 0;
   std::cin >> n;
   std::cin >> m;
   clock_t tStart = clock();
+
   // Finonacci Number
   //std::cout << Fibonacci(n);
   //std::cout << FibonacciFast(n) << std::endl;
+
   // Last digit of a large Finonacci Number
   //std::cout << LastDigitOfFinonacci(n) << std::endl;
+
   // Huge Finonacci Number modulom
-  std::cout << HugeFinonacciModul(n, m) << std::endl;
+  //std::cout << HugeFinonacciModul(n, m) << std::endl;
+
+  // Sum of Fibonacci
+  //std::cout << SumOfFibonacci(n) << std::endl;
+
+  // Patrial Sum of Fibonacci
+  std::cout << PartialSumOfFibonacci(n, m) << std::endl;
+
   std::cout << "Time taken:\n" << (double)(clock() - tStart)/CLOCKS_PER_SEC << " s" << std::endl;
   return 0;
 }
